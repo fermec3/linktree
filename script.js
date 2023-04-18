47e47c82962babc72341661968e898d6152e0b62
Hudson <ferpomota@gmail.com>
2023-04-17 22:40:18 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+  
+    // substituir a imagem
+    if (html.classList.contains("light")) {
+      // se tiver light mode, adicionar a imagem light
+      img.setAttribute("src", "./assets/avatar.png")
+    } else {
+      // set tiver sem light mode, manter a imagem normal
+      img.setAttribute("src", "./assets/dev-profile.png")
+    }
+
+
+    //texto
+    if (html.classList.contains("light")) {
+    var description = document.getElementById("nome-dev");
+    description.textContent = '@orgatomic.';
+
+    } else {
+        var description = document.getElementById("nome-dev");
+        description.textContent = '@mec';
+  }
+
+}
